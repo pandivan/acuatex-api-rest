a087a2028a9a4252393f80ae97c272b3d4aaf76f
Ivan Hernandez <>
2020-12-23 19:32:03 -0500

    Se actualiza script bd

--- bd/Acuatex.sql
+++ bd/Acuatex.sql
@@ -8,15 +8,6 @@ from dbo.grupo_articulos ga
 
 
 
-
-
-
-
-
-
-
-
-
 /******+ ARTICULOS ******/
 
 SELECT  
@@ -26,12 +17,13 @@ a.codigo, SUBSTRING(a.codigo, 0, CHARINDEX('T', a.codigo)+1) as codigo_articulo,
 from dbo.articulos a
 inner join dbo.precios_articulos pa on pa.cod_articulo = a.codigo
 where 1=1
-and a.codigo in ('3002001001T04','3002001002T02','3002001003T08','3003001003T04','3003001005T04','3005004044T18','3005004045T10','3005004047T3','3007002001T12M','3007002001T18M','3007002001T24','3007002001T6-9M','3007002002T12M','3012004001T1','3012005002TL','3012008001TXL','3008007001T14','3004001054TS','3004001058TXL','3005006028T9','3005005086T24','3012008002TXL','3007002066T18','3007002066T12','3007002066T18','3007002066T24','3007002067T12','3007002002T18M','3007002002T24','3007002067T18','3007002067T24','3007002067T6-9','3012005002TM','3012005002TS','3012005002TXL','3012008001TL','3012008001TM','3012008001TS','3012008002TL','3012008002TM','3012008002TS','3004001054TM','3004001054TS','3004001058TL','3004001058TM','3004001058TS')
+--and a.codigo in ('3002001001T04','3002001002T02','3002001003T08','3003001003T04','3003001005T04','3005004044T18','3005004045T10','3005004047T3','3007002001T12M','3007002001T18M','3007002001T24','3007002001T6-9M','3007002002T12M','3012004001T1','3012005002TL','3012008001TXL','3008007001T14','3004001054TS','3004001058TXL','3005006028T9','3005005086T24','3012008002TXL','3007002066T18','3007002066T12','3007002066T18','3007002066T24','3007002067T12','3007002002T18M','3007002002T24','3007002067T18','3007002067T24','3007002067T6-9','3012005002TM','3012005002TS','3012005002TXL','3012008001TL','3012008001TM','3012008001TS','3012008002TL','3012008002TM','3012008002TS','3004001054TM','3004001054TS','3004001058TL','3004001058TM','3004001058TS')
 --and a.codigo like '%3004001058T%'
 and pa.cod_listaprecios = 01
 and pa.precio <> 0
 --and a.grupo in ('DAMA')
 --and a.nombre like '%BUSO HOMBRE AMAC0002%'
+and a.codigo like '%.%'
 order by 1
 ;
 
@@ -175,5 +167,138 @@ imbabura--> ibarra--> zuleta
 
 
 SD622967 -> caso sp
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+  
+ /********************************************************************************************************************
+ * 
+ * DIMENSION CIUDADES
+ * 
+ ********************************************************************************************************************/
+
+SELECT 
+(c.codigo_provincia+c.codigo_ciudad) as ciudades, 
+prov.nombre as provincia,
+c.nombre as ciudad
+from dbo.ciudades c 
+inner join dbo.ciudades prov on prov.codigo_provincia = c.codigo_provincia and prov.codigo_ciudad = ''
+where (c.codigo_provincia+c.codigo_ciudad) = '10002'
+order by c.codigo_provincia, c.codigo_ciudad, c.nombre
+;
+
+
+
+
+
+ /********************************************************************************************************************
+ * 
+ * DIMENSION CLIENTES
+ * 
+ ********************************************************************************************************************/
+
+SELECT 
+CAST(cedula AS numeric) as clientes,
+cedula,
+nombres as cliente,
+direccion,
+correo,
+telefono,
+direccion_entrega
+from dbo.clientes_registro
+--where cedula = '13072207'
+;
+
+
+
+
+  
+
+
+/********************************************************************************************************************
+ * 
+ * DIMENSION ARTICULOS
+ * 
+ ********************************************************************************************************************/
+
+SELECT a.codigo as articulos, 
+a.grupo, 
+a.subgrupo,
+a.nombre as articulo,  
+pa.precio, 
+SUBSTRING(a.codigo, CHARINDEX('T', a.codigo)+1, 5) as talla,
+a.stock 
+from dbo.articulos a 
+inner join dbo.precios_articulos pa on pa.cod_articulo = a.codigo 
+where 1=1
+--and a.codigo in ('3002001001T04','3002001002T02','3002001003T08','3003001003T04','3003001005T04','3005004044T18','3005004045T10','3005004047T3','3007002001T12M','3007002001T18M','3007002001T24','3007002001T6-9M','3007002002T12M','3012004001T1','3012005002TL','3012008001TXL','3008007001T14','3004001054TS','3004001058TXL','3005006028T9','3005005086T24','3012008002TXL','3007002066T18','3007002066T12','3007002066T18','3007002066T24','3007002067T12','3007002002T18M','3007002002T24','3007002067T18','3007002067T24','3007002067T6-9','3012005002TM','3012005002TS','3012005002TXL','3012008001TL','3012008001TM','3012008001TS','3012008002TL','3012008002TM','3012008002TS','3004001054TM','3004001054TS','3004001058TL','3004001058TM','3004001058TS') 
+and pa.cod_listaprecios = 01
+and a.codigo = '3007002067T6-9'
+;
+
+UPDATE dbo.articulos set stock = 18 where codigo = '3007002002T18M';
+
+
+
+
+if('S' = descodificado, RGB(255,0,0),Black())
+
+
+ /********************************************************************************************************************
+ * 
+ * HECHOS PEDIDOS
+ * 
+ ********************************************************************************************************************/
+SELECT *
+from pedido_encabezado p 
+order by p.cliente ;
+
+
+SELECT * 
+from pedido_detalle pd
+;
+
+UPDATE pedido_encabezado set fecha = getdate()-7 where nro_pedido = '1000';
+
+select getdate();
+
+
+
+SELECT 
+p.nro_pedido as pedidos,
+p.cliente as clientes,
+pd.cod_articulo as articulos,
+(p.codprovincia + p.codciudad) as ciudades,
+p.hora,
+pd.cantidad,
+pd.precio_venta as valor,
+p.fecha as fecha_pedido,
+p.fechaDespacho as fecha_despacho,
+p.estado
+from dbo.pedido_encabezado p
+inner join dbo.pedido_detalle pd on pd.nro_pedido = p.nro_pedido
+in
+;
+
+
+
+
+
+
+
+
+
+
+
 
 
